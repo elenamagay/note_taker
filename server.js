488a6fac3f120e45c450f17cfe3fa8cc42e48b94
elenamagay <elenamagay1985@gmail.com>
2021-02-24 22:29:17 -0500

    changes added

--- server.js
+++ server.js
@@ -2,14 +2,13 @@ const fs = require("fs");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 const path = require("path");
-const { json } = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"));
@@ -18,17 +17,17 @@ app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
         if (err) throw err;
         let notes = JSON.parse(data);
-        res.json(notes);      
+        return res.json(notes);      
     });    
 });
 app.get("/api/notes/:id", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
         if (err) throw err;
         let notes = JSON.parse(data);
-        res.json(notes[req.params.id]);      
+        return res.json(notes[req.params.id]);      
     });
 });
 
@@ -36,12 +35,12 @@ app.post("/api/notes", (req, res) => {
     let { title, text } = req.body;
     let  newNote = { title, text, id: uuidv4() };
 
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
         if (err) throw err;
         let note = JSON.parse(data);
         note.push(newNote);
         note = JSON.stringify(newNote);
-        fs.writeFile("./db/db.json", note, err => {
+        fs.writeFile(path.join(__dirname, "./db/db.json"), note, err => {
             if (err) throw err;
         });
         return res.json(newNote);
@@ -49,13 +48,13 @@ app.post("/api/notes", (req, res) => {
 });
 
 app.delete("/api/notes/:id", (req, res) => {
-    fs.readFile("./db/db.json", "utf8", (err, data) => {
+    fs.readFile(path.join(__dirname, "./db/db.json"), (err, data) => {
         if (err) throw err;
         let allNotes = JSON.parse(data);
         let noteDel = allNotes.filter(dNote => dNote.id === req.params.id);
         allNotes.splice(noteDel, 1);
 
-        fs.writeFile("./db/db.json", JSON.stringify(allNotes), err => {
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(allNotes), err => {
             if (err) throw err;
         });
         return res.json({});
